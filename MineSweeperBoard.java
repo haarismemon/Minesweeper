652c5145b62befc9f51cffb307e229d82450ca36
Haaris Memon <haaris.memon@kcl.ac.uk>
2016-10-12 12:34:26 +0100

    Add 10 random mines on the Cell Board

--- MineSweeperBoard.java
+++ MineSweeperBoard.java
@@ -13,6 +13,30 @@ public class MineSweeperBoard {
 				cells[i][j] = new Cell();
 			}
 		}
+	}
+
+	public void newGame() {
+		//loops 10 times		
+		for (int i = 0; i < 10; ++i) {
+			Random random = new Random();
+			int x = random.nextInt(10);
+			int y = random.nextInt(10);
+			
+			//if the current Cell is already a mine
+			if(cells[x][y].isMine()) {
+				//keep looping until the current Cell is not a mine
+				while(cells[x][y].isMine()) {
+					random = new Random();
+					//set x as new random number
+					x = random.nextInt(10);
+					//set y as new random number
+					y = random.nextInt(10);
+				}
+			}
+			
+			//sets the new Cell to have the mine
+			cells[x][y].setMine(true);
+		}
 	}	
 	
 }
